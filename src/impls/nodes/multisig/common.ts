65812a2a0ef31d72e811d1b5e22a987a03b6234d
Stepan Lavrentev <lawrentievsv@gmail.com>
2025-07-01 12:12:33 +0300

    fix: MultisigExecuted is not defined in api

--- src/impls/nodes/multisig/common.ts
+++ src/impls/nodes/multisig/common.ts
@@ -2,9 +2,9 @@ import {IVec} from "@polkadot/types-codec/types/interfaces";
 import {AccountId} from "@polkadot/types/interfaces/runtime/types";
 import {encodeMultiAddress, ethereumEncode, isEthereumAddress, createKeyMulti} from "@polkadot/util-crypto";
 
-export const MultisigExecuted = api.events.multisig.MultisigExecuted
-export const MultisigApproval = api.events.multisig.MultisigApproval
-export const NewMultisig = api.events.multisig.NewMultisig
+export const MultisigExecuted = api.events?.multisig?.MultisigExecuted
+export const MultisigApproval = api.events?.multisig?.MultisigApproval
+export const NewMultisig = api.events?.multisig?.NewMultisig
 
 export function generateMultisigAddress(
     origin: string,
