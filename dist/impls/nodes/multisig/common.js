cc6cb02386fcfdaab32409c5b7a982da416b2887
kogeler <25884155+kogeler@users.noreply.github.com>
2026-02-10 11:38:45 +0200

    chore: include compiled dist for git dependency usage

--- dist/impls/nodes/multisig/common.js
+++ dist/impls/nodes/multisig/common.js
@@ -10,9 +10,9 @@ function generateMultisigAddress(origin, otherSignatories, threshold) {
     const otherSignatoriesAddresses = otherSignatories.map(accountId => accountId.toString());
     const allAddresses = otherSignatoriesAddresses.concat(origin);
     if ((0, util_crypto_1.isEthereumAddress)(origin)) {
-        return (0, util_crypto_1.ethereumEncode)((0, util_crypto_1.createKeyMulti)(allAddresses, threshold).slice(0, 20));
+        return (0, util_crypto_1.ethereumEncode)((0, util_crypto_1.createKeyMulti)(allAddresses.map(a => (0, util_crypto_1.decodeAddress)(a, true)), threshold).slice(0, 20));
     }
     else {
-        return (0, util_crypto_1.encodeMultiAddress)(allAddresses, threshold, api.registry.chainSS58);
+        return (0, util_crypto_1.encodeAddress)((0, util_crypto_1.createKeyMulti)(allAddresses.map(a => (0, util_crypto_1.decodeAddress)(a, true)), threshold), api.registry.chainSS58);
     }
 }
